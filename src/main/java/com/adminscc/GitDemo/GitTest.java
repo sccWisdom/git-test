b1acc6b97cc6a4deea99a5f01d8c9a2129416c72
cc.shi <445404496@qq.com>
2022-02-08 22:34:46 +0800

    push test

--- src/main/java/com/adminscc/GitDemo/GitTest.java
+++ src/main/java/com/adminscc/GitDemo/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello Git4!");
         System.out.println("master modify");
         System.out.println("hot-fix modify");
+        System.out.println("push test");
     }
 }
